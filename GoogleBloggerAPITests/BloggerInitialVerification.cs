8e9dcfab1aee50d6ac1f598661ee2116ae1ceed2
Aditya Shahi <aditya@codeandqa.com>
2017-12-11 00:46:51 -0500

    rename and clean the code

--- GoogleBloggerAPITests/BloggerInitialVerification.cs
+++ GoogleBloggerAPITests/BloggerInitialVerification.cs
@@ -1,26 +1,21 @@
 ﻿using System;
-using System.IO;
 using System.Net;
-using System.Web.Script.Serialization;
-using System.Text;
-using Newtonsoft.Json;
-using System.IO;
-using Microsoft.CSharp;
 using Newtonsoft.Json.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 //We are going to use only public data so users can verify. TO accomplish this, we will use only APIKEY.
 
 namespace GoogleBloggerAPITests
 {
     [TestClass]
-    public class Frst
+    public class BloggerInitialVerification
     {
         //Some required variables
         const string key = "AIzaSyA6mCnYHn3E-KzgOsQ31B0lWDTRORkVLx4";
         const string endPoint = "https://www.googleapis.com/blogger/v3/";
         const string blogID = "2018816985818355058";
-        string jsonString = String.Empty;
+        string reffUrlForGetBlogger = "blogs/" + blogID + "?key=" + key;
+        String jsonString = String.Empty;
+
         [TestMethod]
         public void getBlogs()
         {
@@ -29,15 +24,19 @@ namespace GoogleBloggerAPITests
                 webClient.BaseAddress = endPoint;
                 try
                 {
-                    jsonString = webClient.DownloadString("blogs/" + blogID + "?key=" + key);
-                    JObject rss = JObject.Parse(jsonString);
-                    string rssTitle = (string)rss["posts"]["totalItems"];
-                    Assert.AreEqual("1", rssTitle);
+                    jsonString = webClient.DownloadString(reffUrlForGetBlogger);
+
+                    JObject response = JObject.Parse(jsonString);
+
+                    //Assert Response with kind, name, url, and total posts avialable.
+                    Assert.AreEqual("blogger#blog", (string)response["kind"]);
+                    Assert.AreEqual("CAQ1", (string)response["name"]);
+                    Assert.AreEqual("http://caq10.blogspot.com/", (string)response["url"]);
+                    Assert.AreEqual(1, (int)response["posts"]["totalItems"]);
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
-                    throw;
+                    throw new Exception(e.Message);
                 }
                 
             }       
